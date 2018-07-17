bf91de0cbc8ad409ec4be00368189af5339160b4
Robert <kisielarobert@gmail.com>
2018-07-17 19:06:20 +0100

    Change RelativeLayout to ConstraintLayout. Set up landscape mode layout

--- app/src/main/java/com/kisielarobert/dicee/MainActivity.java
+++ app/src/main/java/com/kisielarobert/dicee/MainActivity.java
@@ -10,7 +10,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
